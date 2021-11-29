6f19cdd3fcb58663aa269f5276257b3ea6d31ddb
Garrett Gu <gu@utexas.edu>
2021-11-28 21:13:30 -0600

    uncomment downloadUTCTFFile

--- src/scripts/beatmap.ts
+++ src/scripts/beatmap.ts
@@ -16,24 +16,24 @@ export default class BeatMap {
         this.notes.push(new Note(time, Math.floor(Math.random() * 4)));
     }
 
-    // downloadFile() {
-    //     const bytes: Array<number> = [];
-    //     this.notes.forEach(note => {
-    //         bytes.push(...note.toBytes());
-    //     });
+    downloadUTCTFFile() {
+        const bytes: Array<number> = [];
+        this.notes.forEach(note => {
+            bytes.push(...note.toBytes());
+        });
         
-    //     const arrayBuffer = new ArrayBuffer(bytes.length);
-    //     const view = new Uint8Array(arrayBuffer);
-    //     for (let i = 0; i < bytes.length; i++) {
-    //         view[i] = bytes[i] as number;
-    //     }
+        const arrayBuffer = new ArrayBuffer(bytes.length);
+        const view = new Uint8Array(arrayBuffer);
+        for (let i = 0; i < bytes.length; i++) {
+            view[i] = bytes[i] as number;
+        }
 
-    //     const blob = new Blob([arrayBuffer], {type: 'application/octet-stream'});
-    //     const link = document.createElement('a');
-    //     link.href = URL.createObjectURL(blob);
-    //     link.download = 'beatmap.map';
-    //     link.click();
-    // }
+        const blob = new Blob([arrayBuffer], {type: 'application/octet-stream'});
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'beatmap.map';
+        link.click();
+    }
 
     downloadFile() {
         var notestr = "";
